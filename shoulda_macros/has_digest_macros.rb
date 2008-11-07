812e36dad2378f55987325b45924e6dbea0f62ca
Matthew Todd <matthew.todd@gmail.com>
2008-11-07 13:18:28 +0300

    Quick shoulda macro.

---
+++ shoulda_macros/has_digest_macros.rb
@@ -0,0 +1,12 @@
+module HasDigest
+  module Shoulda
+    def should_have_digest(name)
+      context "Class #{model_class.name} with digest #{name}" do
+        should_have_db_column name, :type => :string, :limit => 40
+        should_have_instance_methods "digest_#{name}"
+      end
+    end
+  end
+end
+
+Test::Unit::TestCase.extend(HasDigest::Shoulda)
