f23993ec5eaabbc24557209c67f5a88e378aa5a6
Matthew Todd <matthew.todd@gmail.com>
2008-11-07 18:07:42 +0300

    Working on shoulda macro.

--- shoulda_macros/has_digest.rb
+++ shoulda_macros/has_digest.rb
@@ -4,16 +4,19 @@ module HasDigest
     # <tt>has_digest :name</tt> and that the necessary callback methods have
     # been created.
     def should_have_digest(name, options = {})
-      context "Class #{model_class.name} with digest #{name}" do
-        should_have_db_column name, :type => :string # TODO :limit => 40
+      options.assert_valid_keys(:depends)
+
+      context "#{model_class.name} with digest :#{name}" do
+        should_have_db_column name, :type => :string, :limit => 40
         should_have_instance_methods "digest_#{name}"
 
-        dependencies = options[:depends]
-        dependencies = [dependencies] unless dependencies.respond_to?(:each)
-        dependencies.compact!
+        if options[:depends]
+          dependencies = options[:depends]
+          dependencies = [dependencies] unless dependencies.respond_to?(:each)
 
-        dependencies.each do |dependency|
-          should_have_instance_methods "#{dependency}", "#{dependency}="
+          dependencies.each do |dependency|
+            should_have_instance_methods "#{dependency}", "#{dependency}="
+          end
         end
       end
     end
