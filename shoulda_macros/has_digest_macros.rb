77e5f5aaa326014f53b987d7da2bac3d2c82438a
Matthew Todd <matthew.todd@gmail.com>
2008-11-07 14:41:57 +0300

    Beginning documentation, ignore column size for now.

--- shoulda_macros/has_digest_macros.rb
+++ shoulda_macros/has_digest_macros.rb
@@ -2,7 +2,7 @@ module HasDigest
   module Shoulda
     def should_have_digest(name)
       context "Class #{model_class.name} with digest #{name}" do
-        should_have_db_column name, :type => :string, :limit => 40
+        should_have_db_column name, :type => :string # TODO :limit => 40
         should_have_instance_methods "digest_#{name}"
       end
     end
